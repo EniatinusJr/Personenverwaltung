3829040d751556cc80e94d9a6ae54ff3d7a35af6
Pandouby <57749377+Pandouby@users.noreply.github.com>
2022-06-30 16:53:14 +0200

    ez clap

--- Personenverwaltung/src/main/java/ch/bbw/personenverwaltung/MainController.java
+++ Personenverwaltung/src/main/java/ch/bbw/personenverwaltung/MainController.java
@@ -3,7 +3,11 @@ package ch.bbw.personenverwaltung;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -16,8 +20,43 @@ public class MainController {
 
 
     @GetMapping("person")
-    public String homepage(Model model) {
+    public String person(Model model) {
         model.addAttribute("persons", dataRepository.findAll());
-        return "index";
+        return "person";
     }
+
+    @GetMapping("personForm/{id}")
+    public String personFrom(@PathVariable Long id, Model model) {
+        Person person = new Person();
+        person.setBirthdate(LocalDate.now());
+        person.setEmail("test@test.com");
+        person.setName("lololol");
+        person.setLastname("no no");
+
+        if(id > 0){
+            model.addAttribute("person", dataRepository.findById(id).get());
+        } else {
+            model.addAttribute("person", new Person());
+        }
+
+        return "personForm";
+    }
+
+    @GetMapping("person/delete/{id}")
+    public String deletePerson(@PathVariable Long id, Model model) {
+        dataRepository.deleteById(id);
+        return "redirect:/person";
+    }
+
+    @PostMapping("/create/{id}")
+    public String create(@PathVariable Long id, @ModelAttribute Person person){
+        person.setId(id);
+        System.out.println(person.getId());
+        dataRepository.save(person);
+        return "redirect:/person";
+    }
+
+    private void update(Long id){
+    }
+
 }
--- Personenverwaltung/src/main/java/ch/bbw/personenverwaltung/Person.java
+++ Personenverwaltung/src/main/java/ch/bbw/personenverwaltung/Person.java
@@ -1,16 +1,20 @@
 package ch.bbw.personenverwaltung;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 public class Person {
+    @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
     private String name;
     private String lastname;
     private String email;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate birthdate;
     private char gender;
 
